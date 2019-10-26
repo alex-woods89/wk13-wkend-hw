3a07d41125ff0512df73e5e155fec667240ba504
Alex Woods <alex.woods89@hotmail.co.uk>
2019-10-26 21:14:38 +0100

    done

---
+++ src/FeaturesList.js
@@ -0,0 +1,31 @@
+import React from'react'
+import('./FeaturesList.css');
+
+const FeaturesList = ({features}) => {
+
+  const featuresList = features.map((feature, index) => {
+    const { name, description, MDNLink, isImplemented, implementations} = feature
+    return (
+    <ul key={index}>
+      <li>
+        <h3>{name}</h3>
+        <p>{description}</p>
+        <a href={MDNLink}>📖Documentation</a>
+        <h5>Implemented? {isImplemented ? "✅": "❌"}</h5>
+        <p>Possible Implementations:</p> 
+        <p><code>{implementations}</code></p>
+      </li>
+    </ul>
+    )
+  })
+
+  return (
+  <div>
+    {featuresList}
+  </div>
+  )
+
+
+}
+
+export default FeaturesList
